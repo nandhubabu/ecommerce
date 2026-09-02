04bd960b8d50cfd1f618c250671996aa114f7520
nandhubabu <nandhubabuvktd@gmail.com>
2026-09-03 00:29:44 +0530

    feat: add Order model with User foreign key and creation timestamp

--- orders/models.py
+++ orders/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-user = models.ForinKey(User,on_delete = )
+user = models.ForinKey(User,on_delete = models.CASCADE)
+date = models.DateTimeFeild(auto_now_add = True)
