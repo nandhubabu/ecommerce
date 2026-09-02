07e1b9d29e19e1fa8c1b2bb36806b9486d7b9046
nandhubabu <nandhubabuvktd@gmail.com>
2026-09-03 00:56:28 +0530

    feat: add Order and OrderItem models to manage customer purchases

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,14 @@
+from products.models import Product
 from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-user = models.ForinKey(User,on_delete = models.CASCADE)
-date = models.DateTimeFeild(auto_now_add = True)
+class Order(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE)
+    date = models.DateTimeField(auto_now_add = True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,on_delete = models.CASCADE)
+    product = models.ForeignKey(Product,on_delete = models.CASCADE)
+    quantity = models.IntegerField()
+    historic_price = models.DecimalField(max_digits=10, decimal_places=2)
