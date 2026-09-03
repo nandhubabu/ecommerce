5a069bbe5626fdad634e1da0c33a7dff20602fb3
nandhubabu <nandhubabuvktd@gmail.com>
2026-09-03 14:51:17 +0530

    feat: add Product model and implement product list template with Bootstrap styling

--- products/models.py
+++ products/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length = 255)
     price = models.DecimalField(max_digits = 10, decimal_places = 2)
+    photo = models.ImageField(upload_to = 'products/')
