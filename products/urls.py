461eda08f307101a6d87f229c61d79777d4868f2
nandhubabu <nandhubabuvktd@gmail.com>
2026-09-07 14:28:41 +0530

    feat: implement product listing, cart functionality, and associated URL routing

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
+from os import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('add-to-cart/<int:product_id>', views.add_to_cart,name = 'add_to_cart')
+    path('add-to-cart/<int:product_id>', views.add_to_cart,name = 'add_to_cart'),
+    path('cart/',views.cart_view,name = 'cart'),
 
 ]
--- products/views.py
+++ products/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import redirect
-from orders.models import OrderItem
-from django.shortcuts import render
-from .models import Product,Order
+from django.shortcuts import redirect, render
+from orders.models import Order, OrderItem
+from .models import Product
 
 # Create your views here.
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
 
-def add_to_cart(request,product_id):
+def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
-    order, create = Order.objects.get_or_create(user = request.user)
-    order_item , created = OrderItem.objects.get_or_create(product = product ,order  = order )
+    order, created = Order.objects.get_or_create(user=request.user)
+    order_item, created = OrderItem.objects.get_or_create(product=product, order=order)
 
     return redirect('cart')
+
+def cart_view(request):
+    order, created = Order.objects.get_or_create(user=request.user)
+    cart_items = OrderItem.objects.filter(order=order)
+    return render(request, 'products/cart.html', {'cart_items': cart_items})
